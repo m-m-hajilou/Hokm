a4672fe326a4eab50b638f4377cb8214f6556294
m-m-hajilou <m.m.hajilou@gmail.com>
2020-04-24 14:49:41 +0430

    add function random number and add to function divide card

--- hokm.c
+++ hokm.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
 char card [53][4]={"D.1","D.2","D.3","D.4","D.5","D.6","D.7","D.8","D.9","D.10","D.11","D.12","D.13",
 "G.1","G.2","G.3","G.4","G.5","G.6","G.7","G.8","G.9","G.10","G.11","G.12","G.13",
 "P.1","P.2","P.3","P.4","P.5","P.6","P.7","P.8","P.9","P.10","P.11","P.12","P.13",
@@ -15,9 +18,27 @@ char meCard[13][4];
 
 int divideCard()
 {
+  srand(time());
+  int j;
+  for(int i=0;i<53;i++)
+  {
+    j=getRandomsNumber(0,53);
+    cardCopy[i]=card[j];
+    
 
+  }
 }
+
+int getRandomsNumber(int low, int up)
+{
+   return rand()% (up+low+1)+low;
+}
+
 int main(int argc, char const *argv[])
 {
+
+    srand(time());
+    int randomnumber=0+rand()%6;
+    printf("%d\n",randomnumber );
   return 0;
 }

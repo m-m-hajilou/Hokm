fc543a44b0c7159cd4c626726895c5e5159a0e12
m-m-hajilou <m.m.hajilou@gmail.com>
2020-05-06 11:28:58 +0430

    add empty addScoreWinPlayr()

--- hokm.c
+++ hokm.c
@@ -23,6 +23,7 @@
   int determiningRulByCountCardOrTotalRank(int );
   int howDownCard();
   int minusMinusNumbertDownCard();
+  int addScoreWinPlayr(int);
   char card [52][5]={"D.2","D.3","D.4","D.5","D.6","D.7","D.8","D.9","D.10","D.11","D.12","D.13","D.14",
   "G.2","G.3","G.4","G.5","G.6","G.7","G.8","G.9","G.10","G.11","G.12","G.13","G.14",
   "P.2","P.3","P.4","P.5","P.6","P.7","P.8","P.9","P.10","P.11","P.12","P.13","P.14",
@@ -381,6 +382,13 @@ int determiningPointStartCard()
     return 1;
   }
 
+  int addScoreWinPlayr(int player);
+  {
+    switch (player) {
+      case /* value */:
+    }
+
+  }
 
   /*This is the section for displaying the game*/
   int showGame()
